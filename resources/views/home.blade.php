4908831c8f7b11102dfbc628c1e95fbc552103aa
hueyanroberto <hueyan.roberto@gmail.com>
2022-05-04 17:48:49 +0700

    modif timeline

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,81 +11,82 @@
 
 @section('content')
 
-<section class="resume py-5 d-lg-flex justify-content-center align-items-center" id="resume">
-	<div class="container-timeline">
+<section class="resume pt-5 d-lg-flex justify-content-center align-items-center" id="resume">
+	<div class="container container-timeline">
 		<h2 class="d-flex justify-content-center my-5 fs-1 mb-4 myTitle">TIMELINE</h2>
-		<div class="row-content">
-			<div class="col-lg-6 col-md-6 col-12 d-flex">
-				<div class="timeline">
-					<div class="timeline-wrapper">
-						 <div class="timeline-yr">
-							  <span>MEI</span>
-						 </div>
-						 <div class="timeline-info">                         
-                          <br>  <small>20 Mei 2022</small>
-							  <h4><span>Open Registration</span></h4>
-						 </div>
-					</div>
-
-					<div class="timeline-wrapper">
-						<div class="timeline-yr">
-							<span>AGT</span>
-						</div>
-						<div class="timeline-info">
-                            <br><small>22 Agustus 2022</small>
-							<h4><span>Registration Closed</span></h4>
-						</div>                  
-						<div class="timeline-info">
-                           <br> <small>27 Agustus 2022</small>
-							<h4><span>Workshop 1</h4>
-						</div> 
-						<div class="timeline-info">
-                          <br>  <small>28 Agustus 2022</small>
-							<h4><span>Workshop 2</span></h4>
-						</div>
-					</div>
-
-					<div class="timeline-wrapper">
-						<div class="timeline-yr">
-							<span>SEP</span>
-						</div>
-						<div class="timeline-info">
-                            <Br><small>3 September 2022</small>
-							<h3><span>Technical Meeting Penyisihan</span></h3>
-						</div>
-					</div>
-
-					<div class="timeline-wrapper">
-						<div class="timeline-info">
-                        <br><small>4 September 2022</small>
-							<h3><span>Penyisihan</span></h3>
-						</div>
-					</div>
-					<div class="timeline-wrapper">
-						<div class="timeline-info">
-							<small>5 - 9 September 2022</small>
-							<h3><span>Pengerjaan Final</span></h3>
-						</div>
-					</div>
-					<div class="timeline-wrapper">
-						<div class="timeline-info">
-                            <small>10 September 2022</small>
-							<h3><span>Submission & TM Final</span></h3>
-						</div>
-					</div>
-					<div class="timeline-wrapper">
-						<div class="timeline-yr">
-							<span>END</span>
-						</div>
-						<div class="timeline-info">
-                            <small>11 September 2022</small>
-							<h3><span>Final</span></h3>
-						</div>
-					</div>
-				</div>
-				<div class="col-lg-7 col-md-7 col-15 d-flex">
-					<a href="mainweb/images/foto/poster.jpg" target="blank"><img style="width:100%; height:100%; object-fit: contain;" src="{{asset('../mainweb/img/poster/poster.jpg')}}" alt="poster"></a>
-				</div>
+		<div class="row row-content">
+			<div class="col-lg-12 col-md-12 col-sm-12 d-flex">
+                <div class="row">
+                    <div class="timeline col-lg-6 col-md-6 col-sm-12">
+                        <div class="timeline-wrapper">
+                            <div class="timeline-yr">
+                                <span>MEI</span>
+                            </div>
+                            <div style="display: flex; flex-direction:column;">
+                                <div class="timeline-info">                         
+                                    <br><small>20 Mei 2022</small>
+                                    <h4><span>Open Registration</span></h4>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="timeline-wrapper">
+                            <div class="timeline-yr">
+                                <span>AGT</span>
+                            </div>
+                            <div style="display: flex; flex-direction:column;">
+                                <div class="timeline-info">
+                                    <br><small>22 Agustus 2022</small>
+                                    <h4><span>Registration Closed</span></h4>
+                                </div>                  
+                                <div class="timeline-info">
+                                    <br><small>27 Agustus 2022</small>
+                                    <h4><span>Workshop 1</h4>
+                                </div> 
+                                <div class="timeline-info">
+                                    <br><small>28 Agustus 2022</small>
+                                    <h4><span>Workshop 2</span></h4>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="timeline-wrapper">
+                            <div class="timeline-yr">
+                                <span>SEP</span>
+                            </div>
+                            <div style="display: flex; flex-direction:column;">
+                                <div class="timeline-info">
+                                    <Br><small>3 September 2022</small>
+                                    <h4><span>Technical Meeting Penyisihan</span></h4>
+                                </div>
+                                <div class="timeline-info">
+                                    <br><small>4 September 2022</small>
+                                    <h4><span>Penyisihan</span></h4>
+                                </div>
+                                <div class="timeline-info">
+                                    <br><small>5 - 9 September 2022</small>
+                                    <h4><span>Pengerjaan Final</span></h4>
+                                </div>
+                                <div class="timeline-info">
+                                    <br><small>10 September 2022</small>
+                                    <h4><span>Submission & TM Final</span></h4>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="timeline-wrapper">
+                            <div class="timeline-yr">
+                                <span>END</span>
+                            </div>
+                            <div style="display: flex; flex-direction: column;">
+                                <div class="timeline-info">
+                                    <small>11 September 2022</small>
+                                    <h4><span>Final</span></h4>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-md-6 col-sm-12 d-flex mt-3">
+                        <a href="mainweb/img/poster/poster.jpg" target="blank"><img style="width:100%; height:100%; object-fit: contain;" src="{{asset('../mainweb/img/poster/poster.jpg')}}" alt="poster"></a>
+                    </div>
+                </div>
 			</div>
 		</div>
 	</div>
@@ -94,7 +95,7 @@
 <!-- end timeline -->
 
 
-    <div class="container mt-5">
+    <div class="container mt-4">
         <div class="d-flex justify-content-center my-5 fs-1 myTitle" style="color: #A0583A">PRIZES</div>
         <div class="prize-container">
             <div class="row px-2 mx-5">
@@ -115,7 +116,7 @@
                 <div class="d-flex justify-content-center my-2 fs-3" style="color: #ECEECA">Juara 2</div>
                 <div class="d-flex justify-content-center my-2 fw-bolder fs-3" style="color: #FAE14D">Rp 2.500.000</div>
                 <div class="text-center fs-6" style="color: #ECEECA">
-                    Potongan USP sebesar 100% ( Jika masuk Jurusan Teknik Informatika Program Kekhususan Multimedia ) Piala MANIAC + Sertifikat
+                    Potongan USP sebesar 50% ( Jika masuk Jurusan Teknik Informatika Program Kekhususan Multimedia ) Piala MANIAC + Sertifikat
                 </div>
               </div>
               <div class="col py-5 px-3">
@@ -125,7 +126,7 @@
                 <div class="d-flex justify-content-center my-2 fs-3" style="color: #ECEECA">Juara 3</div>
                 <div class="d-flex justify-content-center my-2 fw-bolder fs-3" style="color: #FAE14D">Rp 1.000.000</div>
                 <div class="text-center fs-6" style="color: #ECEECA">
-                    Potongan USP sebesar 100% ( Jika masuk Jurusan Teknik Informatika Program Kekhususan Multimedia ) Piala MANIAC + Sertifikat
+                    Potongan USP sebesar 25% ( Jika masuk Jurusan Teknik Informatika Program Kekhususan Multimedia ) Piala MANIAC + Sertifikat
                 </div>
               </div>
             </div>
