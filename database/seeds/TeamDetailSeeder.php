d64c02127618d771c93841a6db2e96a0ac00e802
Letcher20 <ericklauren86@gmail.com>
2022-05-08 20:46:10 +0700

    Update Added Seeder
    SeederAdded

--- database/seeds/TeamDetailSeeder.php
+++ database/seeds/TeamDetailSeeder.php
@@ -16,7 +16,7 @@ class TeamDetailSeeder extends Seeder
             ['team_id'=>'1' ,'name' => 'Wardi Halim', 'role'=>'Leader', 'phone_number'=>'0992 5481 984', 'email'=>'amelia02@yahoo.co.id','image'=>'img/img']
         );
 
-        DB::table('team_details')->insert(['team_id'=>'1' ,'name' => 'Elon Saputra', 'role'=>'Member', 'phone_number'=>'0967 3637 998', 'email'=>'gantar.rahmawati@gmail.co.id','image'=>'img/img']);
+        DB::table('team_details')->insert(['team_id'=>'1' ,'name' => 'ElonS Saputra', 'role'=>'Member', 'phone_number'=>'0967 3637 998', 'email'=>'gantar.rahmawati@gmail.co.id','image'=>'img/img']);
         DB::table('team_details')->insert(['team_id'=>'1' ,'name' => 'Rangga Putra', 'role'=>'Member', 'phone_number'=>'0627 4559 833', 'email'=>'rizki32@yahoo.co.id','image'=>'img/img']);
     }
 }
