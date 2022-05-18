0b5427b88d3e135b338bccc533b09ab3cf2009e3
hueyanroberto <hueyan.roberto@gmail.com>
2022-05-18 15:04:23 +0700

    add meta tag

--- resources/views/layouts/mainweb.blade.php
+++ resources/views/layouts/mainweb.blade.php
@@ -4,6 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta property="og:title" content="MANIAC XI">
+    <meta property="og:description" content="MANIAC (Multimedia and Interactive Art Competition) merupakan lomba berbasis multimedia untuk siswa SMA/K sederjat yang mencakup rally, dan design games assets. MANIAC diselenggarakan oleh Program Studi Informatika program Multimedia Universitas Surabaya.">
+    <meta property="og:url" content="https://maniac.ifubaya.id/">
+    <meta property="og:image" content="https://maniac.ifubaya.id/mainweb/img/logo/square-logo.png">
     
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
