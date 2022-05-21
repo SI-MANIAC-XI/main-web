3c171fdadd979b960b6b4882cbd87aecbf932934
hueyanroberto <hueyan.roberto@gmail.com>
2022-05-22 00:16:43 +0700

    revisi home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -27,13 +27,28 @@
 </section>
 
 
+<section id="poster" class="mt-5 pt-5">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-xl-6 col-lg-8 col-md-8">
+                <a href="{{ asset('mainweb/img/poster/poster.png') }}" target="blank"><img src="{{ asset('mainweb/img/poster/poster-scroll.png') }}" alt="Scroll" width="100%; height:100%; object-fit: contain;"></a>
+            </div>
+        </div>
+    </div>
+</section>
+
 <section class="resume pt-5 d-lg-flex justify-content-center align-items-center" id="resume">
 	<div class="container container-timeline">
 		<h2 class="d-flex justify-content-center my-5 fs-1 mb-4 myTitle">TIMELINE</h2>
-		<div class="row row-content">
+        <div class="row timeline-big justify-content-center mb-5">
+            <div class="col-xl-10 col-lg-12">
+                <img src="{{ asset('mainweb/img/poster/Timeline.png') }}" alt="Timeline" width="100%">
+            </div>
+        </div>
+		<div class="row row-content timeline-small mb-5">
 			<div class="col-lg-12 col-md-12 col-sm-12 d-flex">
                 <div class="row">
-                    <div class="timeline col-lg-6 col-md-6 col-sm-12">
+                    <div class="timeline col-lg-12 col-md-12 col-sm-12">
                         <div class="timeline-wrapper">
                             <div class="timeline-yr">
                                 <span>MEI</span>
@@ -99,10 +114,10 @@
                             </div>
                         </div>
                     </div>
-                    <div class="col-lg-6 col-md-6 col-sm-12 d-flex mt-3">
+                    {{-- <div class="col-lg-6 col-md-6 col-sm-12 d-flex mt-3">
                        
                         <a href="{{ asset('mainweb/img/poster/poster.png') }}" target="blank"><img style="width:100%; height:100%; object-fit: contain;" src="{{asset('../mainweb/img/poster/poster.png')}}" alt="poster"></a>
-                    </div>
+                    </div> --}}
                 </div>
 			</div>
 		</div>
