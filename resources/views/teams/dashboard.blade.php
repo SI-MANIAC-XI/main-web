599760cb872edb24bc286ae319250d3ba6893a93
Letcher20 <ericklauren86@gmail.com>
2022-05-21 19:35:28 +0700

    Update Team

--- resources/views/teams/dashboard.blade.php
+++ resources/views/teams/dashboard.blade.php
@@ -46,12 +46,13 @@
                         <div class="row mt-3">
                             <label class="text-center label-ganti-kp">Ganti Kartu Pelajar</label>
                             <input type="file" class="myInputFile @error("{{ 'imgAnggota'.$loop->index }}") is-invalid @enderror" accept="application/pdf,image/*" name="{{ 'imgAnggota'.$loop->index }}" id="imgAnggota2">
-                            @error("{{ 'imgAnggota'.$loop->index }}")
-                              <div class="invalid-feedback text-center">
-                                 {{ $message }}
-                              </div>
-                            @enderror
+                           
                         </div>
+                        @error("{{ 'imgAnggota'.$loop->index }}")
+                        <div class="invalid-feedback text-center">
+                           {{ $message }}
+                        </div>
+                      @enderror
                         @endif
                     </div>
                     @endforeach
