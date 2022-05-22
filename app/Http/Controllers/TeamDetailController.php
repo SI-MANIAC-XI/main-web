32456c4aaec26a64e1aa4afb60a7d5e20076efb8
andreas <s160419047@student.ubaya.ac.id>
2022-05-22 14:01:13 +0700

    update alert success & show error team blm fix

--- app/Http/Controllers/TeamDetailController.php
+++ app/Http/Controllers/TeamDetailController.php
@@ -5,89 +5,118 @@ namespace App\Http\Controllers;
 use App\Team;
 use App\TeamDetail;
 use Illuminate\Http\Request;
+//use Illuminate\Support\Facades\Validator;
+use Validator;
 
 class TeamDetailController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
+   /**
+    * Display a listing of the resource.
+    *
+    * @return \Illuminate\Http\Response
+    */
+   public function index()
+   {
+      //
+   }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
+   /**
+    * Show the form for creating a new resource.
+    *
+    * @return \Illuminate\Http\Response
+    */
+   public function create()
+   {
+      //
+   }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
+   /**
+    * Store a newly created resource in storage.
+    *
+    * @param  \Illuminate\Http\Request  $request
+    * @return \Illuminate\Http\Response
+    */
+   public function store(Request $request)
+   {
+      //
+   }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\TeamDetail  $teamDetail
-     * @return \Illuminate\Http\Response
-     */
-    public function show(TeamDetail $teamDetail)
-    {
-        //
-    }
+   /**
+    * Display the specified resource.
+    *
+    * @param  \App\TeamDetail  $teamDetail
+    * @return \Illuminate\Http\Response
+    */
+   public function show(TeamDetail $teamDetail)
+   {
+      //
+   }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\TeamDetail  $teamDetail
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(TeamDetail $teamDetail)
-    {
-        //
-    }
+   /**
+    * Show the form for editing the specified resource.
+    *
+    * @param  \App\TeamDetail  $teamDetail
+    * @return \Illuminate\Http\Response
+    */
+   public function edit(TeamDetail $teamDetail)
+   {
+      //
+   }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\TeamDetail  $teamDetail
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, TeamDetail $teamDetail)
-    {
-        //
-    }
+   /**
+    * Update the specified resource in storage.
+    *
+    * @param  \Illuminate\Http\Request  $request
+    * @param  \App\TeamDetail  $teamDetail
+    * @return \Illuminate\Http\Response
+    */
+   public function update(Request $request, TeamDetail $teamDetail)
+   {
+      //
+   }
 
-    public function updateImg(Request $request, Team $team)
-    {
-        $request->validate([
-            'imgAnggota0' => ['max:1024'],
-            'imgAnggota1' => ['max:1024'],
-            'imgAnggota2' => ['max:1024']
-        ]);
+   public function updateImg(Request $request, Team $team)
+   {
+      //cara 1
+      $request->validate(
+         [
+            'imgAnggota0' => 'file|max:1024',
+            'imgAnggota1' => 'file|max:1024',
+            'imgAnggota2' => 'file|max:1024'
+         ],
+         [
+            'imgAnggota0.file' => 'Pastikan format file Kartu Pelajar dalam bentuk PDF, JPG, PNG, atau JPEG',
+            'imgAnggota0.max' => 'Pastikan ukuran file Kartu Pelajar memiliki ukuran maksimal yaitu 1 MB',
+            'imgAnggota1.file' => 'Pastikan format file Kartu Pelajar dalam bentuk PDF, JPG, PNG, atau JPEG',
+            'imgAnggota1.max' => 'Pastikan ukuran file Kartu Pelajar memiliki ukuran maksimal yaitu 1 MB',
+            'imgAnggota2.file' => 'Pastikan format file Kartu Pelajar dalam bentuk PDF, JPG, PNG, atau JPEG',
+            'imgAnggota2.max' => 'Pastikan ukuran file Kartu Pelajar memiliki ukuran maksimal yaitu 1 MB',
+         ]
+      );
 
-        $team['status'] = "pending";
-        $team['message'] = "";
-        $team->save();
+      // $this->validate($request, [
+      //    'imgAnggota0' => 'file|max:1024',
+      //    'imgAnggota1' => 'file|max:1024',
+      //    'imgAnggota2' => 'file|max:1024'
+      // ]);
+      //cara 2
+      // $validation = Validator::make($request->all(),[
+      //    'imgAnggota0' => 'file|max:1024',
+      //    'imgAnggota1' => 'file|max:1024',
+      //    'imgAnggota2' => 'file|max:1024'
+      // ]);
 
-        $imgFolder = "files";
+      // if($validation->fails()) {
+      //    return redirect()->withErrors($validation)->withInput();
+      // }
+      
+      $team['status'] = "pending";
+      $team['message'] = "";
+      $team->save();
 
-        if ($request->hasFile('imgAnggota0')) {
+      $imgFolder = "files";
+      try {
+         if ($request->hasFile('imgAnggota0')) {
             //ImageMember0
             $idMember0 = $request->get('idAnggota0');
             $member0 = TeamDetail::find($idMember0);
@@ -98,9 +127,9 @@ class TeamDetailController extends Controller
             $member0->image = $imgFile;
 
             $member0->save();
-        }
+         }
 
-        if ($request->hasFile('imgAnggota1')) {
+         if ($request->hasFile('imgAnggota1')) {
             //ImageMember1
             $idMember1 = $request->get('idAnggota1');
             $member1 = TeamDetail::find($idMember1);
@@ -111,9 +140,9 @@ class TeamDetailController extends Controller
             $member1->image = $imgFile;
 
             $member1->save();
-        }
+         }
 
-        if ($request->hasFile('imgAnggota2')) {
+         if ($request->hasFile('imgAnggota2')) {
             //ImageMember2
             $idMember2 = $request->get('idAnggota2');
             $member2 = TeamDetail::find($idMember2);
@@ -124,19 +153,28 @@ class TeamDetailController extends Controller
             $member2->image = $imgFile;
 
             $member2->save();
-        }
+         }
+         //kasih pesan berhasil
+         session()->flash("success", "File Gambar atau PDF Kartu Pelajar berhasil di ubah. Silahkan tunggu hingga panitia memvalidasi ulang data Anda");
+         return redirect()->back();
+      } catch (\Exception $e) {
+         //pesan gagal karena format file tidak sesuai
+         $message = 'Terjadi kesalahan saat upload file Gambar atau PDF.
+                     Pastikan format file dalam bentuk PDF, JPG, PNG, dan JPEG dengan maksimal ukuran file adalah 1 MB. 
+                     Pastikan format nama file adalah NamaTim_Nama';
+         session()->flash("error",$message);
+         return redirect()->back();
+      }
+   }
 
-        return redirect()->back();
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\TeamDetail  $teamDetail
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(TeamDetail $teamDetail)
-    {
-        //
-    }
+   /**
+    * Remove the specified resource from storage.
+    *
+    * @param  \App\TeamDetail  $teamDetail
+    * @return \Illuminate\Http\Response
+    */
+   public function destroy(TeamDetail $teamDetail)
+   {
+      //
+   }
 }
--- resources/views/teams/dashboard.blade.php
+++ resources/views/teams/dashboard.blade.php
@@ -20,52 +20,66 @@
                     <h3>Anggota</h3>
                 </div>
             </div>
+            <div class="row col-lg-12 m-1">
+               @if(session()->has("success"))
+                  <div class="alert alert-success alert-dismissible fade show" role="alert">
+                     <strong>{{ session()->get("success") }}</strong>
+                     <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                  </div>
+               @endif
+               @if(session()->has("error"))
+                  <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                     <strong>{{ session()->get("error") }}</strong>
+                     <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                  </div>
+               @endif
+            </div>
+            
             <form action={{ url('updateteam/'.$team->id) }} method="POST" enctype="multipart/form-data">
                 @csrf
                 @method('PUT')
                 <div class="row mt-2 justify-content-evenly">
                     @foreach ($team->teamDetail as $item)
-                    <input type="hidden" name="{{ 'idAnggota'.$loop->index }}" value="{{ $item->id }}">
-                    <div class="col-lg-4 mb-3 daftar-anggota">
-                        <div class="row mb-3">
-                            <a href="{{ asset('files/'.$item->image) }}" class="btn myBtn" target="_blank" style="width: 80%; margin: 0 auto;">Check Me</a>
-                        </div>
-                        <div class="row justify-content-between">
-                            <div class="col-5">Nama</div>
-                            <div class="col-7">{{ $item->name }}</div>
-                        </div>
-                        <div class="row justify-content-between">
-                            <div class="col-5">No HP</div>
-                            <div class="col-7">{{ $item->phone_number }}</div>
-                        </div>
-                        <div class="row justify-content-between">
-                            <div class="col-5">Email</div>
-                            <div class="col-7">{{ $item->email }}</div>
-                        </div>
-                        @if ($team->status != 'accepted')
-                           <div class="row mt-3">
-                              <label class="text-center label-ganti-kp">Ganti Kartu Pelajar</label>
-                              <input type="file" class="myInputFile @error('{{ "imgAnggota".$loop->index }}') is-invalid @enderror" 
-                              accept="application/pdf,image/*" name="{{ 'imgAnggota'.$loop->index }}" id="imgAnggota2">
+                        <input type="hidden" name="{{ 'idAnggota'.$loop->index }}" value="{{ $item->id }}">
+                        <div class="col-lg-4 mb-3 daftar-anggota">
+                           <div class="row mb-3">
+                              <a href="{{ asset('files/'.$item->image) }}" class="btn myBtn" target="_blank" style="width: 80%; margin: 0 auto;">Check Me</a>
                            </div>
-                           @error('{{ "imgAnggota".$loop->index }}')
-                              <div class="invalid-feedback text-center">
-                                 {{ $message }}
+                           <div class="row justify-content-between">
+                              <div class="col-5">Nama</div>
+                              <div class="col-7">{{ $item->name }}</div>
+                           </div>
+                           <div class="row justify-content-between">
+                              <div class="col-5">No HP</div>
+                              <div class="col-7">{{ $item->phone_number }}</div>
+                           </div>
+                           <div class="row justify-content-between">
+                              <div class="col-5">Email</div>
+                              <div class="col-7">{{ $item->email }}</div>
+                           </div>
+                           @if ($team->status != 'accepted')
+                              <div class="row m-3">
+                                 <label for="imgAnggota2" class="text-center label-ganti-kp">Ganti Kartu Pelajar</label>
+                                 <input type="file" accept="application/pdf,image/*" name="imgAnggota{{ $loop->index }}" id="imgAnggota2 formFileMultiple" class="myInputFile form-control @error('imgAnggota{{ $loop->index }}') is-invalid @enderror" style="color:#a0583a;">
+                                 @error('imgAnggota{{ $loop->index }}')
+                                    <div class="invalid-feedback">
+                                       {{ $message }}
+                                    </div>
+                                 @enderror
                               </div>
-                           @enderror
-                        @endif
-                    </div>
+                           @endif
+                        </div>
                     @endforeach
                 </div>
                 <div class="row mt-3">
                     <div class="col-lg-12 text-center d-flex flex-column justify-content-center align-items-center">
                         <h3>Status</h3>
                         @if ($team->status != 'accepted')
-                        <h4 class="status ditolak" style="font-weight: 700; text-transform: uppercase;">{{ $team->status }}</h4>
-                        <p class="keterangan">{{ $team->message }}</p>
-                        <button type="submit" class="btn myBtn dark">Simpan Perubahan</button>
+                           <h4 class="status ditolak" style="font-weight: 700; text-transform: uppercase;">{{ $team->status }}</h4>
+                           <p class="keterangan">{{ $team->message }}</p>
+                           <button type="submit" class="btn myBtn dark">Simpan Perubahan</button>
                         @else
-                        <h4 class="status diterima" style="font-weight: 700; text-transform: uppercase;">{{ $team->status }}</h4>
+                           <h4 class="status diterima" style="font-weight: 700; text-transform: uppercase;">{{ $team->status }}</h4>
                         @endif
                     </div>
                 </div>
